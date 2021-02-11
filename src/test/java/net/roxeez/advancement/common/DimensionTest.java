765cfa2b1f53de9cce85d986b4f2932b48ed8f43
Roxeez <roxeeztsu@gmail.com>
2021-02-12 00:40:08 +0100

    Missing unit testing modification

--- src/test/java/net/roxeez/advancement/common/DimensionTest.java
+++ src/test/java/net/roxeez/advancement/common/DimensionTest.java
@@ -1,2 +1,25 @@
-package net.roxeez.advancement.common;public class DimensionTest {
+package net.roxeez.advancement.common;
+
+import net.roxeez.advancement.TestBase;
+import net.roxeez.advancement.serialization.ObjectSerializer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DimensionTest extends TestBase {
+
+    private final ObjectSerializer serializer = new ObjectSerializer();
+
+    @Test
+    public void dimension_serialization_should_return_name() {
+        for(Dimension dimension : Dimension.values()) {
+            assertEquals("\""+dimension.getName() + "\"", serializer.serialize(dimension));
+        }
+    }
+
+    @Test
+    public void dimension_get_name_should_return_correct_value() {
+        assertEquals(Dimension.getByName("the_end"), Dimension.THE_END);
+    }
+
 }
--- src/test/java/net/roxeez/advancement/common/PotionTest.java
+++ src/test/java/net/roxeez/advancement/common/PotionTest.java
@@ -1,2 +1,25 @@
-package net.roxeez.advancement.common;public class PotionTest {
+package net.roxeez.advancement.common;
+
+import net.roxeez.advancement.TestBase;
+import net.roxeez.advancement.serialization.ObjectSerializer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PotionTest extends TestBase {
+
+    private final ObjectSerializer serializer = new ObjectSerializer();
+
+    @Test
+    public void potion_serialization_should_return_key() {
+        for(Potion potion : Potion.values()) {
+            assertEquals("\""+ potion.getKey().toString() + "\"", serializer.serialize(potion));
+        }
+    }
+
+    @Test
+    public void potion_get_name_should_return_correct_value() {
+        assertEquals(Potion.getByName("strong_healing"), Potion.INSTANT_HEALTH_II);
+    }
+
 }
